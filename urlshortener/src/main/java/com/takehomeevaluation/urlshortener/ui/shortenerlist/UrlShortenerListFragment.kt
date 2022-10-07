9f79dde069cf581c9cf420e777da70d693911ee3
Frederyk Linhares <Frederyk.Linhares-ext@ab-Inbev.com>
2022-10-07 00:16:31 -0300

    d

--- urlshortener/src/main/java/com/takehomeevaluation/urlshortener/ui/shortenerlist/UrlShortenerListFragment.kt
+++ urlshortener/src/main/java/com/takehomeevaluation/urlshortener/ui/shortenerlist/UrlShortenerListFragment.kt
@@ -1,5 +1,6 @@
 package com.takehomeevaluation.urlshortener.ui.shortenerlist
 
+import android.widget.Toast
 import androidx.core.view.isVisible
 import androidx.core.widget.addTextChangedListener
 import androidx.lifecycle.LifecycleOwner
@@ -32,7 +33,7 @@ class UrlShortenerListFragment :
                 when (it) {
                     is Loading -> setupViewsVisibility(progressBarVisible = true)
                     is Success -> setupViewsVisibility(progressBarVisible = false)
-                    is Error -> setupViewsVisibility(progressBarVisible = false)
+                    is Error -> handleError(error = it)
                 }
             }
 
@@ -44,6 +45,12 @@ class UrlShortenerListFragment :
                 binding.buttonSendUrl.isEnabled = it
             }
         }
+    }
+
+    private fun handleError(error: Error) {
+        val message = error.exception?.message ?: ""
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+        setupViewsVisibility(progressBarVisible = false)
     }
 
     private fun setupAdapter() {
